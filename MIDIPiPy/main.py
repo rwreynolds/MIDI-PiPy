d489c934db1baa3b3e4928cdc2f593d6db065639
6288038 <rwrmail1@gmail.com>
2019-11-23 09:55:42 -0600

    Adding MIDI translation logic

--- MIDIPiPy/main.py
+++ MIDIPiPy/main.py
@@ -48,7 +48,7 @@ from rtmidi.midiconstants import (CHANNEL_PRESSURE, CONTROLLER_CHANGE, NOTE_ON, 
                                   PITCH_BEND, POLY_PRESSURE, PROGRAM_CHANGE)
 
 
-log = logging.getLogger('midi2command')
+log = logging.getLogger('midipipy')
 BACKEND_MAP = {
     'alsa': rtmidi.API_LINUX_ALSA,
     'jack': rtmidi.API_UNIX_JACK,
@@ -67,8 +67,10 @@ STATUS_MAP = {
 
 
 class Command(object):
-    def __init__(self, name='', description='', status=0xB0, channel=None, data=None,
-                 command=None):
+    def __init__(
+        self, name='', description='', status=0xB0, channel=None, 
+        data=None, command=None
+    ):
         self.name = name
         self.description = description
         self.status = status
@@ -83,12 +85,33 @@ class Command(object):
             raise TypeError("Could not parse 'data' field.")
 
 
+class MidiTrans(object):
+    def __init__(
+        self, name='', description='', status=0xB0, channel=None,
+        data=None, translation=None
+    ):
+        self.name = name
+        self.description = description
+        self.status = status
+        self.channel = channel
+        self.translation = translation
+
+        if data is None or isinstance(data, int):
+            self.data = data
+        elif hasattr(data, 'split'):
+            self.data = map(int, data.split())
+        else:
+            raise TypeError("Could not parse 'data' field.")
+
+
 class MidiInputHandler(object):
-    def __init__(self, port, config):
+    def __init__(self, port, cmdconfig, miditrans):
         self.port = port
         self._wallclock = time.time()
         self.commands = dict()
-        self.load_config(config)
+        self.translations = dict()
+        self.load_cmdconfig(cmdconfig)
+        self.load_miditrans(miditrans)
 
     def __call__(self, event, data=None):
         event, deltatime = event
@@ -112,21 +135,54 @@ class MidiInputHandler(object):
         log.debug("[%s] @%i CH:%2s %02X %s %s", self.port, self._wallclock,
                   channel or '-', status, data1, data2 or '')
 
-        # Look for matching mapped data 
+        # Look for matching translation data
         if channel < 16:
             log.info(
-            "Raw MIDI data for mapping: channel - %s, status - %s, data1 - %s, data2 - %s",
-            channel, status, data1, data2)
+                "Raw MIDI data for mapping: channel - %s, status - %s, \
+                data1 - %s, data2 - %s",
+                channel, status, data1, data2
+            )
 
-            midiout = rtmidi.O
+            trans = self.lookup_command(status, channel, data1, data2)
 
+            midiout = rtmidi.MidiOut()
+
+            available_ports = midiout.get_ports()
+
+            if available_ports:
+                mioport = 1
+                midiout.open_port(mioport)
+
+                note_on = [0x90, 96, 112]  # channel 1, middle C, velocity 112
+                note_off = [0x80, 96, 0]
+                mode = [0xF0, 0x42, 0x30, 0x00, 0x01, 0x15, 0x4E, 0x00, 0xF7]
+                bank1 = [0xB0, 0x00, 0x00]
+                bank2 = [0xB0, 0x20, 0x03]
+                prog = [0xC0, 0x00]
+
+                midiout.send_message(mode)
+                time.sleep(.001)
+                midiout.send_message(bank1)
+                time.sleep(.001)
+                midiout.send_message(bank2)
+                time.sleep(.001)
+                midiout.send_message(prog)
+                time.sleep(.001)
+                midiout.close_port()
+            else:
+                midiout.open_virtual_port("My virtual output")
+
+            del midiout
+         
         # Look for matching command definitions
         if channel == 16:         
             cmd = self.lookup_command(status, channel, data1, data2)
 
             log.info(
-            "Raw MIDI data for command: channel - %s, status - %s, data1 - %s, data2 - %s",
-            channel, status, data1, data2)
+                "Raw MIDI data for command: channel - %s, status - %s, \
+                data1 - %s, data2 - %s",
+                channel, status, data1, data2
+            )
 
             if cmd:
                 cmdline = cmd.command % dict(
@@ -150,6 +206,19 @@ class MidiInputHandler(object):
                     cmd.data[0] == data1 and cmd.data[1] == data2):
                 return cmd
 
+    def lookup_translation(self, status, channel, data1, data2):
+        for trans in self.translations.get(status, []):
+            if channel is not None and trans.channel != channel:
+                continue
+
+            if (data1 is None and data2 is None) or trans.data is None:
+                return trans
+            elif isinstance(trans.data, int) and trans.data == data1:
+                return trans
+            elif (isinstance(trans.data, (list, tuple)) and
+                    trans.data[0] == data1 and trans.data[1] == data2):
+                return trans
+
     def do_command(self, cmdline):
         log.info("Calling external command: %s", cmdline)
         try:
@@ -158,7 +227,7 @@ class MidiInputHandler(object):
         except:  # noqa: E722
             log.exception("Error calling external command.")
 
-    def load_config(self, filename):
+    def load_cmdconfig(self, filename):
         if not exists(filename):
             raise IOError("Config file not found: %s" % filename)
 
@@ -187,6 +256,37 @@ class MidiInputHandler(object):
                 log.debug("Config: %s\n%s\n", cmd.name, cmd.description)
                 self.commands.setdefault(status, []).append(cmd)
 
+    def load_miditrans(self, filename):
+        if not exists(filename):
+            raise IOError("MIDI translation file not found: %s" % filename)
+
+        with open(filename) as patch:
+            data = yaml.load(patch)
+
+        for transspec in data:
+            try:
+                if isinstance(transspec, dict) and 'translation' in transspec:
+                    trans = MidiTrans(**transspec)
+                elif len(transspec) >= 2:
+                    trans = MidiTrans(*transspec)
+            except (TypeError, ValueError) as exc:
+                log.debug(transspec)
+                raise IOError("Invalid MIDI translation specification: %s" % exc)
+            else:
+                status = STATUS_MAP.get(trans.status.strip().lower())
+
+                if status is None:
+                    try:
+                        int(trans.status)
+                    except:  # noqa: E722
+                        log.error("Unknown status '%s'. Ignoring command",
+                                  trans.status)
+
+                log.debug("Config: %s\n%s\n", trans.name, trans.description)
+                self.translations.setdefault(status, []).append(trans)
+
+                
+
 
 def main(args=None):
     """Main program function.
@@ -204,8 +304,10 @@ def main(args=None):
          help='MIDI input port name or number (default: open virtual input)')
     padd('-v', '--verbose',
          action="store_true", help='verbose output')
-    padd(dest='config', metavar="CONFIG",
-         help='Configuration file in YAML syntax.')
+    padd(dest='cmdconfig', metavar="CMDCONFIG",
+         help='Command Configuration file in YAML syntax.')
+    padd(dest='miditrans', metavar="MIDITRANS",
+         help='MIDI translation file in YAML syntax.')
 
     args = parser.parse_args(args)
 
@@ -224,8 +326,12 @@ def main(args=None):
     except (EOFError, KeyboardInterrupt):
         return
 
+    # MidiInputHandler(port_name, args.cmdconfig, args.miditrans)
+
     log.debug("Attaching MIDI input callback handler.")
-    midiin.set_callback(MidiInputHandler(port_name, args.config))
+    midiin.set_callback(
+        MidiInputHandler(port_name, args.cmdconfig, args.miditrans)
+    )
 
     log.info("Entering main loop. Press Control-C to exit.")
     try:
