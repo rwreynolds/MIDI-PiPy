08b0a01f9d486595b323046bacb2ebc538bdf3c5
6288038 <rwrmail1@gmail.com>
2019-12-01 22:01:27 -0600

    Added versioning

---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+
+setup(name='MIDIPiPy', version='0.0.01', author='RWReynolds',
+      author_email='rick.reynolds@ebcomputer.net',
+      url='http://ebcomputer.net',)
